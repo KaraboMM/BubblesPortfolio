09053e0378cd14c795e83a3bee326fa16839745d
KaraboMM <karabo2814@gmail.com>
2026-05-06 15:17:37 +0200

    Add files via upload

---
+++ valentines.py
@@ -0,0 +1,18 @@
+print("Hello, Habibi!")
+print("I have a question to ask")
+
+options = ["yes" or "no"]
+
+print("Will you be my Valentine?" '\n')
+
+input("your answer:" '\n')
+   
+if ("yes"):
+  print('\n' "of course" '\n'"you had no other choice" '\n') 
+  
+
+input(" Press Enter to exit")
+
+def on (Enter): exit()
+    
+
